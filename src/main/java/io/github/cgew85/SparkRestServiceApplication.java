27f83115d65a713653fa8b27cc2b6cbf8d424103
cgew85 <christoph.gewehr@mailbox.org>
2016-05-07 22:39:10 +0200

    #setupYolo

---
+++ src/main/java/io/github/cgew85/SparkRestServiceApplication.java
@@ -0,0 +1,12 @@
+package io.github.cgew85;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkRestServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/cgew85/SparkRestServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package io.github.cgew85;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SparkRestServiceApplication.class)
+@WebAppConfiguration
+public class SparkRestServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
