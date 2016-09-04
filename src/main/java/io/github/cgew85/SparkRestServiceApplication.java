0a433ffe6d78f2f5fe045977913d920c68223357
cgew85 <christoph.gewehr@mailbox.org>
2016-09-04 17:13:14 +0200

    #setupYolo

--- src/main/java/io/github/cgew85/SparkRestServiceApplication.java
+++ src/main/java/io/github/cgew85/SparkRestServiceApplication.java
@@ -3,10 +3,19 @@ package io.github.cgew85;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static spark.Spark.*;
+
 @SpringBootApplication
 public class SparkRestServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SparkRestServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SparkRestServiceApplication.class, args);
+
+        get("/hello", (request, response) -> "Hello World");
+
+        get("/hello/:name", (request, response) -> "Hello: " + request.params(":name"));
+
+
+
+    }
 }
